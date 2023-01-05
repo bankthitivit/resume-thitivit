3a4a150f1d6e7b9a032413d31f0d8c5fcc1fc6b7
Bank <bankthitivit.pst@gmail.com>
2023-01-05 13:46:12 +0700

    fix(contact): change image png to svg and layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,45 @@
-import IconsLine from "../images/icons-line.png";
-import IconsTel from "../images/icons-tel.png";
-
 function Contact() {
   return (
     <section
       id="contact"
-      className="pb-32 pt-16 flex flex-col items-center justify-center bg-[#212121]"
+      className="pb-32 pt-0 flex flex-col items-center justify-center bg-[#212121]"
     >
       <div className="flex flex-row items-start justify-start">
-        <div className="bg-[#292C36] w-[80vw] rounded-full pt-10 pb-8 mt-8 flex flex-col items-center justify-center">
-          <h1 className="text-white text-3xl bg-[#86787873] rounded-xl p-5 md:pl-28 pl-10 md:pr-28 pr-10">
+        <div className="bg-[#292C36] w-[60vw] rounded-full pt-10 pb-8 mt-8 flex flex-col items-center justify-center">
+          <h1 className="text-white text-2xl bg-[#86787873]  rounded-xl p-5 md:pl-28 pl-10 md:pr-28 pr-10">
             Contact me
           </h1>
 
           <div className=" mt-1 flex flex-row items-start justify-center">
             <div className=" mt-6 w-50 flex flex-row items-start justify-center">
-              <img src={IconsLine} alt="icons-line" className="w-12" />
-              <span className="text-white mt-2 text-xl"> bankankk</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 50 50"
+                width="50px"
+                height="50px"
+                className="fill-[#a7d0ff73] mr-3"
+                alt="icons-line"
+              >
+                <path d="M 9 4 C 6.24 4 4 6.24 4 9 L 4 41 C 4 43.76 6.24 46 9 46 L 41 46 C 43.76 46 46 43.76 46 41 L 46 9 C 46 6.24 43.76 4 41 4 L 9 4 z M 25 11 C 33.27 11 40 16.359219 40 22.949219 C 40 25.579219 38.959297 27.960781 36.779297 30.300781 C 35.209297 32.080781 32.660547 34.040156 30.310547 35.660156 C 27.960547 37.260156 25.8 38.519609 25 38.849609 C 24.68 38.979609 24.44 39.039062 24.25 39.039062 C 23.59 39.039062 23.649219 38.340781 23.699219 38.050781 C 23.739219 37.830781 23.919922 36.789063 23.919922 36.789062 C 23.969922 36.419063 24.019141 35.830937 23.869141 35.460938 C 23.699141 35.050938 23.029062 34.840234 22.539062 34.740234 C 15.339063 33.800234 10 28.849219 10 22.949219 C 10 16.359219 16.73 11 25 11 z M 23.992188 18.998047 C 23.488379 19.007393 23 19.391875 23 20 L 23 26 C 23 26.552 23.448 27 24 27 C 24.552 27 25 26.552 25 26 L 25 23.121094 L 27.185547 26.580078 C 27.751547 27.372078 29 26.973 29 26 L 29 20 C 29 19.448 28.552 19 28 19 C 27.448 19 27 19.448 27 20 L 27 23 L 24.814453 19.419922 C 24.602203 19.122922 24.294473 18.992439 23.992188 18.998047 z M 15 19 C 14.448 19 14 19.448 14 20 L 14 26 C 14 26.552 14.448 27 15 27 L 18 27 C 18.552 27 19 26.552 19 26 C 19 25.448 18.552 25 18 25 L 16 25 L 16 20 C 16 19.448 15.552 19 15 19 z M 21 19 C 20.448 19 20 19.448 20 20 L 20 26 C 20 26.552 20.448 27 21 27 C 21.552 27 22 26.552 22 26 L 22 20 C 22 19.448 21.552 19 21 19 z M 31 19 C 30.448 19 30 19.448 30 20 L 30 26 C 30 26.552 30.448 27 31 27 L 34 27 C 34.552 27 35 26.552 35 26 C 35 25.448 34.552 25 34 25 L 32 25 L 32 24 L 34 24 C 34.553 24 35 23.552 35 23 C 35 22.448 34.553 22 34 22 L 32 22 L 32 21 L 34 21 C 34.552 21 35 20.552 35 20 C 35 19.448 34.552 19 34 19 L 31 19 z" />
+              </svg>
+              <span className="text-[#ffffff73] mt-2 text-xl"> bankankk</span>
             </div>
 
             <div className=" mt-6 w-50 ml-10 flex flex-row items-start justify-center">
-              <img src={IconsTel} alt="icons-line" className="w-8 mt-2 mr-3" />
-              <span className="text-white mt-2 text-xl"> 062-565-1961</span>
+              <svg
+                className="mt-1 mr-3 fill-[#a7d0ff73]"
+                alt="icons-telephone"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 32 32"
+                width="40px"
+                height="40px"
+              >
+                <path d="M23.45 20.93a3 3 0 0 0-4.25 0l-1.45 1.45a26.14 26.14 0 0 1-4.51-3.62 26.62 26.62 0 0 1-3.62-4.51l1.45-1.45a3 3 0 0 0 0-4.25L8.24 5.73a2.93 2.93 0 0 0-2.11-.88h0A3 3 0 0 0 4 5.73L2.64 7.08A6.37 6.37 0 0 0 1 12.33c.32 3.75 2.78 8.26 6.57 12.06S15.92 30.64 19.67 31a7.87 7.87 0 0 0 .84 0 6.07 6.07 0 0 0 4.41-1.64L26.27 28a3 3 0 0 0 .88-2.13 2.93 2.93 0 0 0-.88-2.11zm1.41 5.66L23.5 27.94a4.57 4.57 0 0 1-3.66 1c-3.25-.28-7.39-2.58-10.81-6S3.31 15.41 3 12.16a4.53 4.53 0 0 1 1-3.66L5.41 7.14a1 1 0 0 1 .71-.29h0a1 1 0 0 1 .71.29L9.66 10a1 1 0 0 1 0 1.41l-2 2a1 1 0 0 0-.16 1.21 27.49 27.49 0 0 0 4.33 5.58 27.49 27.49 0 0 0 5.58 4.33 1 1 0 0 0 1.21-.16l2-2a1 1 0 0 1 1.41 0l2.83 2.83h0a1 1 0 0 1 .29.71A1 1 0 0 1 24.86 26.59zM26.6 5.39A14.92 14.92 0 0 0 16 1a1 1 0 1 0 0 2A13 13 0 0 1 29 16.11a1 1 0 0 0 1 1h0a1 1 0 0 0 1-1A14.91 14.91 0 0 0 26.6 5.39z" />
+                <path d="M20.91,11.11a6,6,0,0,1,1.77,4.31,1,1,0,0,0,1,1h0a1,1,0,0,0,1-1,8,8,0,0,0-8-8.1,1,1,0,1,0,0,2A6,6,0,0,1,20.91,11.11Z" />
+              </svg>
+              <span className="text-[#ffffff73] mt-2 text-xl">
+                062-565-1961
+              </span>
             </div>
           </div>
         </div>
